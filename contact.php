0f6e221e38e88221e01ec3fabccf4d11f07feb1b
cmanalo2 <83484743+cmanalo2@users.noreply.github.com>
2021-05-06 23:00:09 -0500

    Final contact.php

--- contact.php
+++ contact.php
@@ -1,8 +1,8 @@
  /* Carlo Manalo send an email with PHP
-       CIS 181 Final Project Update 1 */
+       CIS 181 Final Project */
 <?php 
 if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
+    $to = "cmanalo2@student.ccc.edu"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $phone = $_POST['phone'];
@@ -16,6 +16,6 @@ if(isset($_POST['submit'])){
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    You can also use header('Location: index.html'); // to redirect to another page.
     }
 ?>
